6e62ee5908a4cd129e3e7281fe75d66aeab5d48f
Francois Vessaz <francois.vessaz@lotaris.com>
2014-11-18 15:09:57 +0100

    First complete implementation of to do list

--- js/todolist.js
+++ js/todolist.js
@@ -2,32 +2,26 @@ var app = angular.module('ToDoList', []);
 
 app.controller('ToDoListController', ['$scope', '$http', function($scope, $http) {
 
-	// get todos
-	$scope.todos = [
-		{
-			"id": 1,
-			"name": "test1",
-			"checked": false
-		},
-		{
-			"id": 2,
-			"name": "test2",
-			"checked": true
-		},
-		{
-			"id": 3,
-			"name": "test3",
-			"checked": false
-		}
-	];
+	// get all todos
+	$http.get('/tl/api/list').success(function(data) {
+		$scope.todos = data;
+	});
 
+	// add a new todo
 	$scope.addToDo = function() {
-		// create todo
-		$scope.toDoName = '';
+		$http.post('/tl/api/list', {name: $scope.toDoName}).success(function(data) {
+			$scope.todos.push(data);
+			$scope.toDoName = '';
+		});
 	}
 
+	// update a todo
 	$scope.updateToDo = function(toDo) {
-		// update todo
+		if (toDo.checked) {
+			$http.post('/tl/api/list/' + toDo.id + '/check');
+		} else {
+			$http.post('/tl/api/list/' + toDo.id + '/uncheck');
+		}
 	}
 
 }]);
