7e408a1dcf33bdc7170568233aff9709bc8d21e5
Francois Vessaz <francois.vessaz@lotaris.com>
2014-11-19 17:04:15 +0100

    sort by todo or completed (fronted only)

--- js/todolist.js
+++ js/todolist.js
@@ -1,19 +1,50 @@
-var app = angular.module('ToDoList', []);
+var app = angular.module('ToDoList', ['ui.bootstrap']);
 
 app.controller('ToDoListController', ['$scope', '$http', function($scope, $http) {
 
+	var todoList = [];
+
+	// refresh the displayed list
+	$scope.$watch('showCategory', function() {
+		var todos = [];
+		var completed = [];
+		$scope.nbTodo = 0;
+		$scope.nbCompleted = 0;
+
+		for (var i in todoList) {
+			var todo = todoList[i];
+			if (todo.checked) {
+				completed.push(todo);
+				$scope.nbCompleted += 1;
+			} else {
+				todos.push(todo);
+				$scope.nbTodo += 1;
+			}
+		}
+
+		if ($scope.showCategory === 'all') {
+			$scope.toDisplay = todoList;
+		} else if ($scope.showCategory === 'todo') {
+			$scope.toDisplay = todos;
+		} else if ($scope.showCategory === 'completed') {
+			$scope.toDisplay = completed;
+		}
+	});
+
 	// get all todos
 	$http.get('/tl/api/list').success(function(data) {
-		$scope.todos = data;
+		todoList = data;
+		$scope.showCategory = 'all';
 	});
 
 	// add a new todo
 	$scope.addToDo = function() {
 		$http.post('/tl/api/list', {name: $scope.toDoName}).success(function(data) {
-			$scope.todos.push(data);
+			todoList.push(data);
+			$scope.showCategory = 'all';
 			$scope.toDoName = '';
 		});
-	}
+	};
 
 	// update a todo
 	$scope.updateToDo = function(toDo) {
@@ -22,6 +53,11 @@ app.controller('ToDoListController', ['$scope', '$http', function($scope, $http)
 		} else {
 			$http.post('/tl/api/list/' + toDo.id + '/uncheck');
 		}
-	}
+	};
+
+	// delete completed todos
+	$scope.removeCompleted = function() {
+		
+	};
 
 }]);
