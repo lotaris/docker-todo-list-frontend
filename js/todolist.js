f52491b0bd2a0e6b7d525d6b31075c8e26baab5b
Francois Vessaz <francois.vessaz@lotaris.com>
2014-11-20 15:27:32 +0100

    footer with last identifier

--- js/todolist.js
+++ js/todolist.js
@@ -1,6 +1,6 @@
 var app = angular.module('ToDoList', ['ui.bootstrap']);
 
-app.controller('ToDoListController', ['$scope', '$http', function($scope, $http) {
+app.controller('ToDoListController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
 
 	$scope.todoList = [];
 	$scope.showCategory = 'all';
@@ -20,36 +20,43 @@ app.controller('ToDoListController', ['$scope', '$http', function($scope, $http)
 	});
 
 	// get all todos
-	$http.get('/tl/api/todos').success(function(data) {
+	$http.get('/tl/api/todos').success(function(data, status, headers) {
 		$scope.todoList = data;
+		$rootScope.$emit('RESPONSE_HEADERS', headers);
 	});
 
 	// add a new todo
 	$scope.addToDo = function() {
-		$http.post('/tl/api/todos', {name: $scope.toDoName}).success(function(data) {
+		$http.post('/tl/api/todos', {name: $scope.toDoName}).success(function(data, status, headers) {
 			$scope.todoList.push(data);
 			$scope.nbTodo += 1;
 			$scope.toDoName = '';
+			$rootScope.$emit('RESPONSE_HEADERS', headers);
 		});
 	};
 
 	// update a todo
 	$scope.updateToDo = function(toDo) {
 		if (toDo.checked) {
-			$http.post('/tl/api/todos/' + toDo.id + '/check');
-			$scope.nbTodo -= 1;
-			$scope.nbCompleted += 1;
+			$http.post('/tl/api/todos/' + toDo.id + '/check').success(function(data, status, headers) {
+				$scope.nbTodo -= 1;
+				$scope.nbCompleted += 1;
+				$rootScope.$emit('RESPONSE_HEADERS', headers);
+			});
 		} else {
-			$http.post('/tl/api/todos/' + toDo.id + '/uncheck');
-			$scope.nbTodo += 1;
-			$scope.nbCompleted -= 1;
+			$http.post('/tl/api/todos/' + toDo.id + '/uncheck').success(function(data, status, headers) {
+				$scope.nbTodo += 1;
+				$scope.nbCompleted -= 1;
+				$rootScope.$emit('RESPONSE_HEADERS', headers);
+			});
 		}
 	};
 
 	// delete completed todos
 	$scope.removeCompleted = function() {
-		$http.delete('/tl/api/todos?type=completed').success(function(data) {
+		$http.delete('/tl/api/todos?type=completed').success(function(data, status, headers) {
 			$scope.todoList = data;
+			$rootScope.$emit('RESPONSE_HEADERS', headers);
 		});
 	};
 
@@ -64,5 +71,13 @@ app.controller('ToDoListController', ['$scope', '$http', function($scope, $http)
 		}
 		return false;
 	}
+
+}]);
+
+app.controller('FooterController', ['$scope', '$rootScope', function($scope, $rootScope) {
+
+	$rootScope.$on('RESPONSE_HEADERS', function(event, headers) {
+		$scope.lastIdentifier = headers('X-Server-Identifier');
+	});
 
 }]);
